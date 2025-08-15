343deb228c06cd2e5b6b7ff8144a2963445b92bb
srrogerio <srrogerio@gmail.com>
2025-08-15 11:55:47 -0300

    terminei por enquanto

--- JavaScript/Dia-001/script.js
+++ JavaScript/Dia-001/script.js
@@ -1,2 +1,41 @@
-// Variáveis
+// Comportamentos estranhos, como todos esses exemplos aqui abaixo retornando true
+console.log( false == '0' );
+console.log( null == undefined );
+console.log( " \t\r\n" == 0 );
+console.log( ' ' == 0 );
 
+
+// Variáveis
+let numeroUm = 1;
+let stringUm = '1';
+let numeroTrinta = 30;
+let stringTrinta = '30';
+let numeroDez = 10;
+let stringDez = '10';
+
+// Exibindo Comparações em JavaScript
+
+// Usando '==' (igualdade solta)
+// Compara o valor, mas não o tipo.
+if (numeroUm == stringUm) {
+console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+}
+
+
+// Usando '===' (igualdade estrita)
+// Compara tanto o valor quanto o tipo.
+if (numeroTrinta === stringTrinta) {
+  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+}
+
+
+// Outro exemplo com '=='
+if (numeroDez == stringDez) {
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+}
